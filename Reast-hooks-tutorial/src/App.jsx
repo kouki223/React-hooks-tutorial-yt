1149b06ad5808cb4a7bb31bcc8f1b56e25c3af30
kouki223 <kouki.1818194@gmail.com>
2025-02-01 01:09:28 +0900

    docs: useReducerみた

--- Reast-hooks-tutorial/src/App.jsx
+++ Reast-hooks-tutorial/src/App.jsx
@@ -1,11 +1,23 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState, useReducer } from "react";
 import "./App.css";
 import KoukicodeContext from "./main";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "increment":
+      return state + 1;
+      case "decrement":
+        return state - 1;
+        default:
+          return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
   const koukicodeInfo = useContext(KoukicodeContext);
   const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
   const handoleClick = () => {
     setCount(count + 1);
@@ -18,6 +30,9 @@ function App() {
   const handleRef = () => {
     console.log(ref.current.value);
   }
+
+  //useMemo
+  
 
   return (
    <div className="App">
@@ -34,6 +49,15 @@ function App() {
     <h1>useRef</h1>
     <input type="text" ref={ref} />
     <button onClick={handleRef}>UseRef</button>
+
+    <hr/>
+    <h1>useReducer</h1>
+    <p>カウント：{state}</p>
+    <button onClick={() => dispatch({ type: "increment" })}>+</button>
+    <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+
+    <hr/>
+    <h1>useMemo</h1>
    </div>
   );
 }
